9f503e9f3985e3c4925d08481ed54448538e7235
mymi14s <mymi14s@hotmail.com>
2023-12-03 20:48:04 +0000

    fix: redirect' git push

--- its/utils.py
+++ its/utils.py
@@ -5,3 +5,10 @@ def get_student():
     if frappe.db.exists("Student", {'user':frappe.session.user}):
         return frappe.get_doc('Student',{'user':frappe.session.user})
     return None
+
+@frappe.whitelist(allow_guest=1)
+def is_authenticated():
+    if frappe.session.user=='Guest':
+        return False
+    else:
+        return True
