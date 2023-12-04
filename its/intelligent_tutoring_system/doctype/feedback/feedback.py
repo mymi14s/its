b29c37496786c4bd0f6fe0ad979086e1e2a55013
mymi14s <mymi14s@hotmail.com>
2023-12-04 23:26:39 +0000

    fix: feedback

--- its/intelligent_tutoring_system/doctype/feedback/feedback.py
+++ its/intelligent_tutoring_system/doctype/feedback/feedback.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2023, Anthony Emmanuel and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Feedback(Document):
-	pass
+	def before_insert(self):
+		if not self.student:
+			student = frappe.get_doc("Student", frappe.db.get_value("Course Enrollment", {
+				'name':self.course_enrollment
+			}, field="student"))
+			self.student = student
